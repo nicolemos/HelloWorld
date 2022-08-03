f3ecdca7c01165f0a647a9cd1f309da53ac642e9
Nicolas Lemos <nlemos@rec.utn.edu.ar>
2022-08-03 20:56:00 -0300

    Actualizado con mod 20:55

--- pages/tests.py
+++ pages/tests.py
@@ -6,21 +6,41 @@ Replace this with more appropriate tests for your application.
 """
 
 import django
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# TODO: Configure your database in settings.py and sync before running tests.
+#TEST PAGINA HOME
+class HomePageTests(SimpleTestCase):
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
 
-class SimpleTest(TestCase):
-    """Tests for the application views."""
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_template_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response,'<h1>Homepage</h1>')
 
-    # Django requires an explicit setup() when running tests in PTVS
-    @classmethod
-    def setUpClass(cls):
-        super(SimpleTest, cls).setUpClass()
-        django.setup()
+# TEST PAGINA ABOUT
+class AboutPageTest(SimpleTestCase):
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'about.html')
+    
+    def test_template_content(self):
+        response = self.client.get(reverse('about'))
+        self.assertContains(response,'<h1>About page</h1>')
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse("Primera APP de prueba")
+#def homePageView(request):
+#    return HttpResponse("Primera APP de prueba")
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
