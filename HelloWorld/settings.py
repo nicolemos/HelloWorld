a1f6cc441c596cb5d2d4ab8b4787401028dd4127
Nico <nlemos@rec.utn.edu.ar>
2022-08-04 19:26:34 -0300

    New updated for heroku deployment

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0f03e7c1-4763-4487-8992-3e63bddbb339'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
